bc6203133d0553a6e3dbd1e42c74cbdc3859955a
plyght <plyght@peril.lol>
2025-05-30 12:29:27 -0400

    true e2ee

--- apps/server/src/db/schema/auth.ts
+++ apps/server/src/db/schema/auth.ts
@@ -7,7 +7,6 @@ export const user = pgTable("user", {
 	emailVerified: boolean("email_verified").notNull(),
 	image: text("image"),
 	publicKey: text("public_key"),
-	serverPrivateKey: text("server_private_key"),
 	createdAt: timestamp("created_at").notNull(),
 	updatedAt: timestamp("updated_at").notNull(),
 });
